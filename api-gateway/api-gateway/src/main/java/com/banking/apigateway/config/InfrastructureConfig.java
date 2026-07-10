babdac9fca2bad1139fd3d987878ac0ea6c2e856
unknown <wilmer_11_90@hotmail.com>
2026-07-10 00:39:54 -0500

    feat: changes

--- api-gateway/api-gateway/src/main/java/com/banking/apigateway/config/InfrastructureConfig.java
+++ api-gateway/api-gateway/src/main/java/com/banking/apigateway/config/InfrastructureConfig.java
@@ -52,7 +52,7 @@ public class InfrastructureConfig {
     @Value("${security.jwt.secret}")
     private String jwtSecret;
 
-    @Bean
+    /*@Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(csrf -> csrf.disable());
         http.authorizeExchange(exchanges -> exchanges
@@ -60,6 +60,16 @@ public class InfrastructureConfig {
                 .anyExchange().authenticated());
         http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
         return http.build();
+    }*/
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
+                )
+                .build();
     }
 
     @Bean
